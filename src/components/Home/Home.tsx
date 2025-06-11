5a2f37bcf9cb9161e389173fbcba81edca3c9bb4
jhonatan <jhonatanbl1999@gmail.com>
2025-06-11 06:20:45 -0300

    site com api

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,47 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import styles from "./Home.module.css";
+import Image from "next/image";
+
+type Product = {
+  id: string,
+  title: string,
+  description: string,
+  price: number,
+  url_img: string
+}
+
+
 
 export default function Home() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+
+  useEffect(() => {
+    async function fetchProducts(){
+      try {
+        
+        const res = await fetch("https://apizinha-psi.vercel.app/api/products");
+
+        const data = await res.json();
+
+        setProducts(data);
+        
+        console.log(data);
+
+      } catch (error) {
+        console.error("fudeu2",error);
+      }
+    }
+
+    fetchProducts();
+  }, []
+
+)
+
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.logoHome}>
@@ -14,6 +53,14 @@ export default function Home() {
           muted
           playsInline
         />
+      </div>
+      <div>
+        {products.map((product) => (
+          <div key={product.id} className={styles.product}>
+            {product.title}
+            <Image src={product.url_img} alt="asdasfa" width={300} height={300} />
+          </div>
+        ) )}
       </div>
     </div>
   );
